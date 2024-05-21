bf66c001c55172cc78d659d7bafcb80f7130b88a
Tomeu Vizoso <tomeu@tomeuvizoso.net>
2024-05-21 14:21:13 +0200

    WIP: rocket driver

---
+++ drivers/accel/rocket/rocket_device.h
@@ -0,0 +1,88 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright 2024 Tomeu Vizoso <tomeu@tomeuvizoso.net> */
+
+#ifndef __ROCKET_DEVICE_H__
+#define __ROCKET_DEVICE_H__
+
+#include <drm/gpu_scheduler.h>
+#include <linux/mutex_types.h>
+
+#define MAX_NUM_CORES 3
+
+/* Taken verbatim from downstream driver */
+/* TODO: Switch to the definitions generated from the TRM in the Mesa tree */
+#define RKNN_OFFSET_VERSION 0x0
+#define RKNN_OFFSET_VERSION_NUM 0x4
+#define RKNN_OFFSET_PC_OP_EN 0x8
+#define RKNN_OFFSET_PC_DATA_ADDR 0x10
+#define RKNN_OFFSET_PC_DATA_AMOUNT 0x14
+#define RKNN_OFFSET_PC_TASK_CONTROL 0x30
+#define RKNN_OFFSET_PC_DMA_BASE_ADDR 0x34
+
+#define RKNN_OFFSET_INT_MASK 0x20
+#define RKNN_OFFSET_INT_CLEAR 0x24
+#define RKNN_OFFSET_INT_STATUS 0x28
+#define RKNN_OFFSET_INT_RAW_STATUS 0x2c
+
+#define RKNN_OFFSET_CLR_ALL_RW_AMOUNT 0x8010
+#define RKNN_OFFSET_DT_WR_AMOUNT 0x8034
+#define RKNN_OFFSET_DT_RD_AMOUNT 0x8038
+#define RKNN_OFFSET_WT_RD_AMOUNT 0x803c
+
+#define RKNN_OFFSET_ENABLE_MASK 0xf008
+
+#define RKNN_INT_CLEAR 0x1ffff
+
+#define RKNN_CNA_FEATURE_GROUP_0	0
+#define RKNN_CNA_FEATURE_GROUP_1	1
+#define RKNN_CNA_WEIGHT_GROUP_0	2
+#define RKNN_CNA_WEIGHT_GROUP_1	3
+#define RKNN_CNA_CSC_GROUP_0		4
+#define RKNN_CNA_CSC_GROUP_1		5
+#define RKNN_CORE_GROUP_0		6
+#define RKNN_CORE_GROUP_1		7
+#define RKNN_DPU_GROUP_0		8
+#define RKNN_DPU_GROUP_1		9
+#define RKNN_PPU_GROUP_0		10
+#define RKNN_PPU_GROUP_1		11
+#define RKNN_DMA_READ_ERROR		12
+#define RKNN_DMA_WRITE_ERROR		13
+
+struct rocket_compatible {
+	int num_resets;
+	const char * const *reset_a_names;
+	const char * const *reset_h_names;
+
+	int num_pm_domains;
+	const char * const *pm_domain_names;
+
+	int num_irqs;
+	const char * const *irq_names;
+
+	int num_iomem;
+};
+
+struct rocket_device {
+	struct device *dev;
+	struct drm_device *ddev;
+	struct platform_device *pdev;
+
+	const struct rocket_compatible *comp;
+
+	struct clk_bulk_data *clks;
+	int num_clks;
+
+	struct reset_control *srst_a[MAX_NUM_CORES];
+	struct reset_control *srst_h[MAX_NUM_CORES];
+
+	void __iomem *iomem[MAX_NUM_CORES];
+
+	struct device *pm_domain_devs[MAX_NUM_CORES];
+	struct device_link *pm_domain_links[MAX_NUM_CORES];
+};
+
+int rocket_device_init(struct rocket_device *rdev);
+void rocket_device_fini(struct rocket_device *rdev);
+void rocket_device_reset(struct rocket_device *rdev);
+
+#endif
---
+++ drivers/accel/rocket/rocket_drv.c
@@ -0,0 +1,187 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Copyright 2024 Tomeu Vizoso <tomeu@tomeuvizoso.net> */
+
+#include <linux/of.h>
+#include <linux/module.h>
+#include <linux/platform_device.h>
+#include <linux/pm_runtime.h>
+#include <drm/drm_drv.h>
+#include <drm/drm_ioctl.h>
+#include <drm/drm_accel.h>
+#include <drm/drm_gem.h>
+
+#include "rocket_drv.h"
+#include "rocket_device.h"
+
+static const char * const rk3588_pm_domains[] = { "npu0", "npu1", "npu2" };
+static const char * const rk3588_resets_a[] = { "srst_a0", "srst_a1", "srst_a2" };
+static const char * const rk3588_resets_h[] = { "srst_h0", "srst_h1", "srst_h2" };
+static const char * const rk3588_irqs[] = { "npu0_irq", "npu1_irq", "npu2_irq" };
+static const struct rocket_compatible rk3588_data = {
+	.num_pm_domains = ARRAY_SIZE(rk3588_pm_domains),
+	.pm_domain_names = rk3588_pm_domains,
+	.num_resets = ARRAY_SIZE(rk3588_resets_a),
+	.reset_a_names = rk3588_resets_a,
+	.reset_h_names = rk3588_resets_h,
+	.num_irqs = ARRAY_SIZE(rk3588_irqs),
+	.irq_names = rk3588_irqs,
+	.num_iomem = 3,
+};
+
+static int
+rocket_open(struct drm_device *dev, struct drm_file *file)
+{
+	struct rocket_device *rdev = dev->dev_private;
+	struct rocket_file_priv *rocket_priv;
+	int ret;
+
+	rocket_priv = kzalloc(sizeof(*rocket_priv), GFP_KERNEL);
+	if (!rocket_priv)
+		return -ENOMEM;
+
+	rocket_priv->rdev = rdev;
+	file->driver_priv = rocket_priv;
+
+	return 0;
+}
+
+static void
+rocket_postclose(struct drm_device *dev, struct drm_file *file)
+{
+	struct rocket_file_priv *rocket_priv = file->driver_priv;
+
+	kfree(rocket_priv);
+}
+
+static const struct drm_ioctl_desc rocket_drm_driver_ioctls[] = {
+#define ROCKET_IOCTL(n, func, flags) \
+	DRM_IOCTL_DEF_DRV(ROCKET_##n, rocket_ioctl_##func, flags)
+};
+
+static const struct file_operations rocket_drm_driver_fops = {
+	.owner = THIS_MODULE,
+	DRM_ACCEL_FOPS,
+};
+
+/*
+ * Rocket driver version:
+ * - 1.0 - initial interface
+ */
+static const struct drm_driver rocket_drm_driver = {
+	.driver_features	= DRIVER_COMPUTE_ACCEL,
+	.open			= rocket_open,
+	.postclose		= rocket_postclose,
+	.ioctls			= rocket_drm_driver_ioctls,
+	.num_ioctls		= ARRAY_SIZE(rocket_drm_driver_ioctls),
+	.fops			= &rocket_drm_driver_fops,
+	.name			= "rocket",
+	.desc			= "rocket DRM",
+	.date			= "20240521",
+	.major			= 1,
+	.minor			= 0,
+};
+
+static int rocket_probe(struct platform_device *pdev)
+{
+	struct rocket_device *rdev;
+	struct drm_device *ddev;
+	int err;
+
+	/* Looks like these assertions have to be inside a function to work */
+	BUILD_BUG_ON(rk3588_data.num_pm_domains > MAX_NUM_CORES);
+	BUILD_BUG_ON(rk3588_data.num_resets > MAX_NUM_CORES);
+	BUILD_BUG_ON(rk3588_data.num_irqs != MAX_NUM_CORES);
+
+	rdev = devm_kzalloc(&pdev->dev, sizeof(*rdev), GFP_KERNEL);
+	if (!rdev)
+		return -ENOMEM;
+
+	rdev->pdev = pdev;
+	rdev->dev = &pdev->dev;
+
+	platform_set_drvdata(pdev, rdev);
+
+	rdev->comp = of_device_get_match_data(&pdev->dev);
+	if (!rdev->comp)
+		return -ENODEV;
+
+	/* Allocate and initialize the DRM device. */
+	ddev = drm_dev_alloc(&rocket_drm_driver, &pdev->dev);
+	if (IS_ERR(ddev))
+		return PTR_ERR(ddev);
+
+	ddev->dev_private = rdev;
+	rdev->ddev = ddev;
+
+	err = rocket_device_init(rdev);
+	if (err) {
+		if (err != -EPROBE_DEFER)
+			dev_err(&pdev->dev, "Fatal error during NPU init\n");
+		goto err_drm_dev;
+	}
+
+	pm_runtime_set_active(rdev->dev);
+	pm_runtime_mark_last_busy(rdev->dev);
+
+	/* TODO: Remove this once it works */
+	pm_runtime_get(rdev->dev);
+	pm_runtime_get(rdev->dev);
+
+	pm_runtime_enable(rdev->dev);
+	pm_runtime_set_autosuspend_delay(rdev->dev, 50); /* ~3 frames */
+	pm_runtime_use_autosuspend(rdev->dev);
+
+
+	/*
+	 * Register the DRM device with the core and the connectors with
+	 * sysfs
+	 */
+	err = drm_dev_register(ddev, 0);
+	if (err < 0)
+		goto err_pm_runtime;
+
+	return 0;
+
+err_pm_runtime:
+	pm_runtime_disable(rdev->dev);
+	rocket_device_fini(rdev);
+	pm_runtime_set_suspended(rdev->dev);
+err_drm_dev:
+	drm_dev_put(ddev);
+	return err;
+}
+
+static void rocket_remove(struct platform_device *pdev)
+{
+	struct rocket_device *rdev = platform_get_drvdata(pdev);
+	struct drm_device *ddev = rdev->ddev;
+
+	drm_dev_unregister(ddev);
+
+	pm_runtime_get_sync(rdev->dev);
+	pm_runtime_disable(rdev->dev);
+	rocket_device_fini(rdev);
+	pm_runtime_set_suspended(rdev->dev);
+
+	drm_dev_put(ddev);
+}
+
+static const struct of_device_id dt_match[] = {
+	{ .compatible = "rockchip,rk3588-rknpu", .data = &rk3588_data, },
+	{}
+};
+MODULE_DEVICE_TABLE(of, dt_match);
+
+static struct platform_driver rocket_driver = {
+	.probe = rocket_probe,
+	.remove_new = rocket_remove,
+	.driver	 = {
+		.name = "rocket",
+		.of_match_table = dt_match,
+	},
+};
+module_platform_driver(rocket_driver);
+
+MODULE_LICENSE("GPL v2");
+MODULE_DESCRIPTION("DRM driver for the Rockchip NPU IP");
+MODULE_AUTHOR("Tomeu Vizoso");
