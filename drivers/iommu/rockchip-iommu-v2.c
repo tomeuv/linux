fcba9984cb27d6e73f4a2913c5b006d786600ed2
Tomeu Vizoso <tomeu@tomeuvizoso.net>
2024-06-04 15:05:13 +0200

    port to 6.10

--- drivers/iommu/rockchip-iommu-v2.c
+++ drivers/iommu/rockchip-iommu-v2.c
@@ -1497,7 +1497,7 @@ static bool rk_iommu_is_attach_deferred(struct device *dev)
 }
 
 static int rk_iommu_of_xlate(struct device *dev,
-			     struct of_phandle_args *args)
+			     const struct of_phandle_args *args)
 {
 	struct platform_device *iommu_dev;
 	struct rk_iommudata *data;
